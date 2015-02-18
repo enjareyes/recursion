a49bcffdab19138c7c722c53821880672b0d468d
Enja Reyes <enja.reyes@gmail.com>
2015-02-18 09:48:16 -0600

    completed first recursion problem

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -45,33 +45,50 @@
 var getElementsByClassName = function(className){
   var elements = [];
   var index = 0;
-  var all = document.body.childNodes;
+  // var all = document.body.childNodes;  // or .children   why not grabbing children's children? 
+  var all = document.body.getElementsByTagName('*') //grabs all elements in body (including nested)
+  all = Array.prototype.slice.apply(all);
+
+  // console.log(all)
+  // console.log(all instanceof Array)
+
+  if ($('body').hasClass(className)) {
+    elements.push(document.body);
+  };
 
   var recursiveFunc = function() {
     if (index === all.length) { 
-      if ($('body').hasClass(className)) {
-        elements.unshift(document.body)
-      }
-      console.log(elements);
+        // var original = elements 
+        // elements=[]
+        // elements.push(document.body)
+        // for (var i=0; i <= original.length-1; i++) {
+        //   elements.push(original[i]);
+        // }
+      // }
+      // console.log(elements);
+      // console.log(elements instanceof Array)
       return elements;
     }
 
     var currentElement = all[index]
+    // console.log(index)
 
-    if ($(currentElement).hasClass(className)) { // if ($(currentElement).classList.contains(className)) {   ??
+    if ($(currentElement).hasClass(className)) { // 
+    // if (currentElement.classList.contains(className)) {   
       elements.push(currentElement);
     }
     
     index += 1; 
-    recursiveFunc();
+    return recursiveFunc();
   }
   
-  recursiveFunc();
+  return recursiveFunc();
 
 };
 
 
-
+// var result = getElementsByClassName('targetClassName');
+// console.log(result);
 
 
 
