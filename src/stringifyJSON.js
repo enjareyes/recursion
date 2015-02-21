fe056281d29fd53229dee143bc86aeb6ddd2b97b
Enja Reyes <enja.reyes@gmail.com>
2015-02-21 11:23:57 -0600

    YAY got stringify working

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,162 +3,109 @@
 
 // but you don't so you're going to write it from scratch:
 
-// var stringifyJSON = function(obj) {
-//   var result;
-
-//   console.log(obj)
-
-//   if (obj!== undefined) {
-//     if (typeof obj == 'string') {
-//       result = '"' + obj +  '"'
-//       console.log('result: ' + result)
-//       return result;
-
-//     } else if (obj instanceof Array) {
-
-//       if (obj.length >= 1) {
-//         var inner = []
-//         for (var i = 0; i < obj.length; i++) {
-//           if (typeof obj[i] == 'string') {
-//             var current = '"' + obj[i] + '"';
-//             inner.push(current)
-//           } else {
-//             inner.push(obj[i].toString())
-//           }
-//         }
-//         result = String("[" + inner + "]");
-//       }
-
-//       if (inner === undefined) {
-//         result = '[]'
-//       }
-//       console.log("result: " + result)
-//       return result;
-
-
-//     } else {
-//       var result = String(obj);
-//       console.log('result: ' + result)
-//       return result
-//     };
-//   } 
-// };
-
 
 var stringifyJSON = function(obj) {
-  var result;
+  var result = ""
 
-  console.log('*********')
   console.log(obj)
+
+  if (obj === undefined || typeof obj === 'function' || obj === null) {
+    return 'null'
+  };
 
   if (obj!== undefined) {
 
     if (obj instanceof Array) {
-      if (obj.length < 1) {
-        result = '[]'; 
-        console.log('Array result ' + result)
-        console.log(result)
-        return result;
-      }
+      result += '[';
 
-      var arrayContents = [];
-      var currentIndex = 0
+      if (obj.length >= 1) {
+        for (var currentIndex=0; currentIndex < obj.length; currentIndex ++) {
+          var currentElem = obj[currentIndex];
 
-      var recursiveCheckArray = function(obj) {
-      
-        var currentElem = obj[currentIndex];
-        
-        if (currentIndex === obj.length){
-          console.log('arrayContents: ' + arrayContents)
-          result = "[" + arrayContents + "]"
-          return result
-        }
-
-        if (typeof currentElem === 'string') {
-          currentElem = '"' + currentElem + '"'; 
-          arrayContents.push(currentElem);
-          currentIndex += 1;
-          return recursiveCheckArray(obj);
-        } 
-
-        if (typeof currentElem === 'number') {
-          arrayContents.push(String(currentElem));
-          currentIndex += 1
-          return recursiveCheckArray(obj)
-        };
-
-        if (currentElem instanceof Array) {   
-          if (currentElem.length > 1) {
-            arrayContents.push('['); //how to remove commah after
-            for (var i = 0; i < currentElem.length; i++) {
-              if (currentElem[i] instanceof Array && currentElem[i].length < 1) {
-                arrayContents.push('[]')
-                currentIndex +=1;
-              } else {
-                arrayContents.push('"' + currentElem[i] + '"')
-                //return recursiveCheckArray(currentElem)
-              }
-            }
-            arrayContents.push(']'); //how to remove comma before this is pushed?
-            currentIndex +=1
-            return recursiveCheckArray(currentElem)
+          if (typeof currentElem === 'number') { 
+            result += String(currentElem);
           }
 
-          // for (var i = 0; i < currentElem.length; i++) {
-          //   if (currentElem[i].length < 1) {
-          //     arrayContents.push('[]')
-          //     return recursiveCheckArray(currentElem);
-          //   }
-          // }
-          //return recursiveCheckArray(currentElem)
-        }
-        return recursiveCheckArray(obj);
-      }
+          if (typeof currentElem === 'string') {
+            result += '"' + currentElem + '"'
+          }
 
-      //if (currentIndex < obj.length) {
-      return recursiveCheckArray(obj);
-      //}
+          if (currentElem instanceof Array) {
+            result += stringifyJSON(currentElem);
+          }
+
+          if (typeof currentElem === 'object' && !(Array.isArray(currentElem))) {
+            result += stringifyJSON(currentElem);
+          }
+         
+          if (currentIndex != obj.length-1) {
+            result += ','
+          }
+        } 
+      }
+        result += "]" 
     }
 
-    if (typeof obj === 'object' && !(Array.isArray(obj))) { //is an obj but not an array
-      result = String(obj)
-      console.log('bool/num result: ' + result)
-      console.log(result)
-      return result;
+    if (typeof obj === 'object' && !(Array.isArray(obj))) { 
+      result += '{';
+
+      var arrayOfObjects = []
+      for (var the_key in obj) {
+        arrayOfObjects.push(the_key);
+      }
+
+      if (obj !== {}) {
+        for (var key in obj) {
+          var currentValue = obj[key];
+
+          if (currentValue !== undefined && typeof currentValue !== 'function') {
+
+            result += '"' + key + '":';
+
+            if (typeof currentValue === 'number' || typeof currentValue === 'boolean') { 
+              result += String(currentValue);
+            }
+
+            if (typeof currentValue === 'string') {
+              result += ('"' + currentValue + '"');
+            }
+
+            if (typeof currentValue === 'object') {
+              result += stringifyJSON(currentValue);
+            }
+
+            if (currentValue === 'null') {
+              result += "null"
+            }
+           
+            if (key !== arrayOfObjects[arrayOfObjects.length-1]) { //if not last prop in object
+              result += ','
+            }
+          }
+        }
+      }
+
+      result += "}" 
+      console.log('object result: ' + result)
     }
 
 
     if (typeof obj === 'boolean' || typeof obj === "number") {
-      result = String(obj)
+      result += String(obj);
       console.log('bool/num result: ' + result)
-      console.log(result)
       return result;
     }
 
     if (typeof obj === 'string') {
-      result = '"' + obj +  '"'
+      result += '"' + obj +  '"'
       console.log('string result: ' + result)
-      console.log(result)
       return result;
     } 
 
-  } else {
-    return null;
-  }
+    return result;
+
+  } 
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
